6a8e40ee6256ad96e3d1b258f7a827b6cbb20b0b
leobrowaldh <leobrowaldh@gmail.com>
2025-02-09 15:06:35 +0100

    httplogging, rest client, swagger

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,6 +1,9 @@
 //IOutputFormatter, OutputFormatter
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Packt.Shared;
+using Northwind.WebApi.Repositories;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Microsoft.AspNetCore.HttpLogging; 
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +33,14 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+//http logging can impact performance, as do W3CLogger
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096; // default is 32k
+    options.ResponseBodyLogLimit = 4096; // default is 32k
+});
 
 var app = builder.Build();
 
@@ -37,8 +48,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json",
+            "Northwind Service API Version 1");
+        c.SupportedSubmitMethods(new[] {
+            SubmitMethod.Get, SubmitMethod.Post,
+            SubmitMethod.Put, SubmitMethod.Delete });
+    });
 }
+
+app.UseHttpLogging();
 
 app.UseHttpsRedirection();
 
